945e7d786a6bd0aa5504f1ce2ffdf4a5cf49f3c4
Tony <tbruseva@yahoo.com>
2023-03-13 14:27:33 +0200

    TicTacToeResponseDto - GameID to Id

--- Tic Tac Toe Web API/Models/Dtos/TicTacToeGameResponseDto.cs
+++ Tic Tac Toe Web API/Models/Dtos/TicTacToeGameResponseDto.cs
@@ -4,7 +4,7 @@ namespace Tic_Tac_Toe_Web_API.Models.Dtos
 {
     public class TicTacToeGameResponseDto
     {
-        public int GameId { get; set; }
+        public int Id { get; set; }
         public Mark[] Grid { get; set; } = new Mark[9];
         public Player? PlayerX { get; set; }
         public Player? PlayerO { get; set; }
