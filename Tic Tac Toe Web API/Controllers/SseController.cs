8b199326f5ffe23ede8d94852a36a20bb46aafef
Tony <tbruseva@yahoo.com>
2023-03-31 17:48:26 +0300

    Implement server-sent event

---
+++ Tic Tac Toe Web API/Controllers/SseController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using Tic_Tac_Toe_Web_API.Managers.Interfaces;
+using Tic_Tac_Toe_Web_API.Models.Mappers;
+
+namespace Tic_Tac_Toe_Web_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SseController : Controller
+    {
+        private IGameManager _gameManager;
+
+        public SseController(IGameManager gameManager)
+        {
+            _gameManager = gameManager;
+
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetEvents(int gameId)
+        {
+            var game = await _gameManager.GetGameByIdAsync(gameId);
+            var gameState = 0;
+
+            Response.Headers.Add("Content-Type", "text/event-stream");
+
+            if (gameState != game.GameState)
+            {
+                gameState = game.GameState;
+                
+                var eventData = $"event: GameState\n" +
+                                $"data: {gameState}\n"; ;
+                var eventDataBytes = Encoding.UTF8.GetBytes(eventData);
+                Response.Body.Write(eventDataBytes, 0, eventDataBytes.Length);
+                Response.Body.Flush();
+            }
+
+            return new EmptyResult();
+        }
+    }
+}
---
+++ Tic Tac Toe Web API/ServerEventsWorker.cs
@@ -0,0 +1,52 @@
+﻿using Lib.AspNetCore.ServerSentEvents;
+using System.Security.Cryptography;
+
+namespace Tic_Tac_Toe_Web_API
+{
+    public class ServerEventsWorker : IHostedService
+    {
+        private readonly IServerSentEventsService client;
+
+        public ServerEventsWorker(IServerSentEventsService client)
+        {
+            this.client = client;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    var clients = client.GetClients();
+                    if (clients.Any())
+                    {
+                        Number.Value = RandomNumberGenerator.GetInt32(1, 100);
+                        await client.SendEventAsync(
+                            new ServerSentEvent
+                            {
+                                Id = "number",
+                                Type = "number",
+                                Data = new List<string>
+                                {
+                                Number.Value.ToString()
+                                }
+                            },
+                            cancellationToken
+                        );
+                    }
+
+                    await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+                }
+            }
+            catch (TaskCanceledException)
+            {
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
