58ff5099b2837a6abb0ecbbdd4debf5d28c366f8
Tony <tbruseva@yahoo.com>
2023-04-06 16:43:42 +0300

    Create DbContext class and TicTacToeDb

---
+++ Tic Tac Toe Web API/Database Models/PlayerDbModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe_Web_API.Database_Models
+{
+    public class PlayerDbModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Game { get; set; }
+        public int Wins { get; set; }
+    }
+}
---
+++ Tic Tac Toe Web API/Migrations/20230406110258_Initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tic_Tac_Toe_Web_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Game = table.Column<string>(type: "TEXT", nullable: false),
+                    Wins = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
